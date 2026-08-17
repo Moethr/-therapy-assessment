ec5be7a107a023149657785d6f13e6b2bc5ea6fc
Moethr <9nr898pgv8@privaterelay.appleid.com>
2026-08-17 23:41:46 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moethr-v9-1-20260817-10';
+const CACHE_NAME = 'moethr-v9-2-2026-08-17-11';
 const SCOPE = self.registration.scope;
 const url = path => new URL(path, SCOPE).toString();
 const APP_SHELL = [
